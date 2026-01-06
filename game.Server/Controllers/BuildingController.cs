9b67087413423f17cf750b5dc661c31b9574a8db
simon-bursa <simon.bursa.023@pslib.cz>
2026-01-06 16:05:25 +0100

    fix?

--- game.Server/Controllers/BuildingController.cs
+++ game.Server/Controllers/BuildingController.cs
@@ -2,6 +2,7 @@
 using game.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using EFCore.BulkExtensions;
 
 namespace game.Server.Controllers
 {
@@ -51,8 +52,7 @@ namespace game.Server.Controllers
                 await _bulkSemaphore.WaitAsync();
                 try
                 {
-                    await _context.Buildings.AddRangeAsync(newBuildings);
-                    await _context.SaveChangesAsync();
+                    await _context.BulkInsertAsync(newBuildings);
                 }
                 finally
                 {
