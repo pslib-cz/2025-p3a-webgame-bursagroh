44843c722dd8960ad532567ea92e46bbebef7bf9
Filip-Groh <filip.groh.023@pslib.cz>
2026-02-03 19:06:12 +0100

    restaurant

--- game.client/src/components/Burger.tsx
+++ game.client/src/components/Burger.tsx
@@ -1,21 +1,58 @@
 import React from 'react'
-import type { Recipe } from '../types/api/models/recipe'
+import type { IngredienceType, Recipe } from '../types/api/models/recipe'
+import Asset from './SVG/Asset'
+import type { AssetType } from '../types/asset'
+import styles from './burger.module.css'
 
 type BurgerProps = {
     burger: Recipe
 }
 
+const mapIngredienceToAssetType = (ingredienceType: IngredienceType): AssetType => {
+    switch (ingredienceType) {
+        case 'Meat':
+            return "meat"
+        case 'Salad':
+            return "salad"
+        case 'BunUp':
+            return "bun_up"
+        case 'BunDown':
+            return "bun_down"
+        case 'Tomato':
+            return "tomato"
+        case 'Sauce':
+            return "sauce"
+        case 'Bacon':
+            return "bacon"
+        case 'Cheese':
+            return "cheese"
+    }
+}
+
 const Burger: React.FC<BurgerProps> = ({ burger }) => {
-  return (
-    <div>
-        {burger.ingrediences.sort((a, b) => a.order - b.order).map((ingredience, index) => (
-            <span key={index}>
-                {ingredience.order}. {ingredience.ingredienceType}
-            </span>
-        ))}
-        <span>{burger.name}</span>
-    </div>
-  )
+    const [offset, setOffset] = React.useState(0)
+
+    const ingrediencePixelHeight = 16
+    const height = burger.ingrediences.length * (ingrediencePixelHeight + offset) + 3 * ingrediencePixelHeight
+
+    const handleEnter = () => {
+        setOffset(64)
+    }
+
+    const handleLeave = () => {
+        setOffset(0)
+    }
+
+    return (
+        <div className={styles.burgerContainer}>
+            <svg onMouseEnter={handleEnter} onMouseLeave={handleLeave} width={ingrediencePixelHeight * 8} height={height} viewBox={`0 0 ${ingrediencePixelHeight * 8} ${height}`} xmlns="http://www.w3.org/2000/svg">
+                {burger.ingrediences.sort((a, b) => b.order - a.order).map((ingredience, index) => (
+                    <Asset key={index} x={0} y={height - ingrediencePixelHeight * 8 - (ingrediencePixelHeight + offset) * index} width={ingrediencePixelHeight * 8} height={ingrediencePixelHeight * 8} assetType={mapIngredienceToAssetType(ingredience.ingredienceType)} />
+                ))}
+            </svg>
+            <span className={styles.burgerName}>{burger.name}</span>
+        </div>
+    )
 }
 
 export default Burger
