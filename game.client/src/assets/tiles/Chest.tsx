e95984c17ae968b50599e639dd102b0a63ecb012
Filip-Groh <filip.groh.023@pslib.cz>
2026-02-04 17:47:10 +0100

    chest added

---
+++ game.client/src/assets/tiles/Chest.tsx
@@ -0,0 +1,75 @@
+import React from "react"
+import { assetTypeToId } from "../../utils/asset"
+
+const Chest: React.FC = () => {
+    return (
+        <svg id={assetTypeToId("chest")} x={0} y={0} width="100%" height="100%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <rect width="64" height="64" fill="black" />
+            <rect x="64" width="64" height="64" fill="black" />
+            <rect x="128" width="64" height="64" fill="black" />
+            <rect x="192" width="64" height="64" fill="black" />
+            <rect x="256" width="64" height="64" fill="black" />
+            <rect x="320" width="64" height="64" fill="black" />
+            <rect x="384" width="64" height="64" fill="black" />
+            <rect x="448" width="64" height="64" fill="black" />
+            <rect y="64" width="64" height="64" fill="black" />
+            <rect x="64" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="128" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="192" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="256" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="320" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="384" y="64" width="64" height="64" fill="#53300E" />
+            <rect x="448" y="64" width="64" height="64" fill="black" />
+            <rect y="128" width="64" height="64" fill="black" />
+            <rect x="64" y="128" width="64" height="64" fill="#53300E" />
+            <rect x="128" y="128" width="64" height="64" fill="#53300E" />
+            <rect x="192" y="128" width="64" height="64" fill="white" />
+            <rect x="256" y="128" width="64" height="64" fill="white" />
+            <rect x="320" y="128" width="64" height="64" fill="#53300E" />
+            <rect x="384" y="128" width="64" height="64" fill="#53300E" />
+            <rect x="448" y="128" width="64" height="64" fill="black" />
+            <rect y="192" width="64" height="64" fill="black" />
+            <rect x="64" y="192" width="64" height="64" fill="black" />
+            <rect x="128" y="192" width="64" height="64" fill="black" />
+            <rect x="192" y="192" width="64" height="64" fill="white" />
+            <rect x="256" y="192" width="64" height="64" fill="white" />
+            <rect x="320" y="192" width="64" height="64" fill="black" />
+            <rect x="384" y="192" width="64" height="64" fill="black" />
+            <rect x="448" y="192" width="64" height="64" fill="black" />
+            <rect y="256" width="64" height="64" fill="black" />
+            <rect x="64" y="256" width="64" height="64" fill="#53300E" />
+            <rect x="128" y="256" width="64" height="64" fill="#53300E" />
+            <rect x="192" y="256" width="64" height="64" fill="white" />
+            <rect x="256" y="256" width="64" height="64" fill="white" />
+            <rect x="320" y="256" width="64" height="64" fill="#53300E" />
+            <rect x="384" y="256" width="64" height="64" fill="#53300E" />
+            <rect x="448" y="256" width="64" height="64" fill="black" />
+            <rect y="320" width="64" height="64" fill="black" />
+            <rect x="64" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="128" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="192" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="256" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="320" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="384" y="320" width="64" height="64" fill="#53300E" />
+            <rect x="448" y="320" width="64" height="64" fill="black" />
+            <rect y="384" width="64" height="64" fill="black" />
+            <rect x="64" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="128" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="192" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="256" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="320" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="384" y="384" width="64" height="64" fill="#53300E" />
+            <rect x="448" y="384" width="64" height="64" fill="black" />
+            <rect y="448" width="64" height="64" fill="black" />
+            <rect x="64" y="448" width="64" height="64" fill="black" />
+            <rect x="128" y="448" width="64" height="64" fill="black" />
+            <rect x="192" y="448" width="64" height="64" fill="black" />
+            <rect x="256" y="448" width="64" height="64" fill="black" />
+            <rect x="320" y="448" width="64" height="64" fill="black" />
+            <rect x="384" y="448" width="64" height="64" fill="black" />
+            <rect x="448" y="448" width="64" height="64" fill="black" />
+        </svg>
+    )
+}
+
+export default Chest
