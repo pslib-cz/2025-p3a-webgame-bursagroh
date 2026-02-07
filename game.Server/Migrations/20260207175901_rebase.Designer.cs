e08a6908fd81766c772a02db747834549e839c8b
Filip-Groh <filip.groh.023@pslib.cz>
2026-02-07 19:42:12 +0100

    door fix

--- game.Server/Migrations/20260207175901_rebase.Designer.cs
+++ game.Server/Migrations/20260207175901_rebase.Designer.cs
@@ -11,7 +11,7 @@ using game.Server.Data;
 namespace game.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260207165144_rebase")]
+    [Migration("20260207175901_rebase")]
     partial class rebase
     {
         /// <inheritdoc />
